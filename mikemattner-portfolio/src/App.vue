1461f682fd3fe01178d6c73bcfa00c0e442703a1
mmattner <mmattner@aislerocket.com>
2019-01-08 10:59:23 -0500

    Adding fonts, and beginning style guide.

--- mikemattner-portfolio/src/App.vue
+++ mikemattner-portfolio/src/App.vue
@@ -9,20 +9,14 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import '@/assets/scss/app.scss';
 #nav {
   padding: 30px;
   a {
-    font-weight: bold;
     color: #2c3e50;
     &.router-link-exact-active {
       color: #42b983;
+      font-weight: bold;
     }
   }
 }
--- mikemattner-portfolio/src/components/TypeSheet.vue
+++ mikemattner-portfolio/src/components/TypeSheet.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="hello">
         <h1>{{ msg }}</h1>
+        <p>Exploring the styles and treatments for my future resume/portfolio.</p>
+        <hr>
+        <h1>H1 sample</h1>
+        <h2>H2 sample</h2>
+        <h3>H3 sample</h3>
+        <h4>H4 sample</h4>
+        <h5>H5 sample</h5>
+        <h6>H6 sample</h6>
+        <p>Brunch vinyl post-ironic before they sold out, organic meggings mlkshk Shoreditch lomo ennui flannel cliche Neutra.  Tonx fap forage Pinterest bitters YOLO, dreamcatcher Austin letterpress High Life chambray mumblecore street art chillwave.</p>
+        <p>Narwhal ugh seitan Pitchfork, put a bird on it irony sartorial Bushwick Marfa viral. Carles plaid tattooed dreamcatcher jean shorts tofu, butcher flannel try-hard.</p>
     </div>
 </template>
 
