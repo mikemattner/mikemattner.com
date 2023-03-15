c6afc762311cec2fa7d062428e3830ad57c966cd
mikemattner <info@mikemattner.com>
2023-03-15 19:48:47 -0400

    Adding image component.

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,22 @@
 <template>
   <main>
     <div class="home-layout flow">
+      <BaseImage src="/images/mike-profile-two.jpg" class="profile-image" />
       <h1 class="maximal-heading home-hello">Hello</h1>
-      <p class="lede content-area">
-        I&rsquo;m Mike Mattner, a creative developer from Michigan with over 20 years of experience working with
-        multidisciplinary teams in a variety of roles. I&rsquo;m currently working at AccuLynx as a Sr. UI Engineer.
-      </p>
-      <p class="content-area">
-        <a href="https://www.linkedin.com/in/mikeamattner/">LinkedIn</a>,
-        <a href="https://mastodon.online/@mikemattner" rel="me">Mastodon</a>,
-        <a href="https://codepen.io/mikemattner/#">Codepen</a>, &amp;
-        <a href="https://github.com/mikemattner">GitHub</a>.
-      </p>
-      <div class="content-area button-group">
-        <BaseButton to="/about" variant="outline" color="primary">Learn More About Me</BaseButton>
+      <div class="home-greeting flow">
+        <p class="lede">
+          I&rsquo;m Mike Mattner, a creative developer from Michigan with over 15 years of experience working with
+          multidisciplinary teams in a variety of roles. I&rsquo;m currently working at AccuLynx as a Sr. UI Engineer.
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/mikeamattner/">LinkedIn</a>,
+          <a href="https://mastodon.online/@mikemattner" rel="me">Mastodon</a>,
+          <a href="https://codepen.io/mikemattner/#">Codepen</a>, &amp;
+          <a href="https://github.com/mikemattner">GitHub</a>.
+        </p>
+        <div class="button-group">
+          <BaseButton to="/about" variant="outline" color="primary">Learn More About Me</BaseButton>
+        </div>
       </div>
       <hr />
       <h2 class="content-area">Lorem Testum</h2>
@@ -22,16 +25,16 @@
         cum delectus porro odio ab, cumque fugiat natus ut consequatur mollitia error at eligendi.
       </p>
       <div class="content-area button-group">
-        <BaseButton to="/about" variant="solid" color="primary">About</BaseButton>
-        <BaseButton to="/about" variant="outline" color="primary">About</BaseButton>
-        <BaseButton to="/about" variant="text" color="primary" is-narrow>About</BaseButton>
-        <BaseButton to="/about" variant="link" color="primary">About</BaseButton>
+        <BaseButton to="/about" variant="solid" size="sm" color="primary">About</BaseButton>
+        <BaseButton to="/about" variant="outline" size="sm" color="primary">About</BaseButton>
+        <BaseButton to="/about" variant="text" size="sm" color="primary" is-narrow>About</BaseButton>
+        <BaseButton to="/about" variant="link" size="sm" color="primary">About</BaseButton>
       </div>
       <div class="content-area button-group">
-        <BaseButton to="/about" variant="solid" color="secondary">About</BaseButton>
-        <BaseButton to="/about" variant="outline" color="secondary">About</BaseButton>
-        <BaseButton to="/about" variant="text" color="secondary" is-narrow>About</BaseButton>
-        <BaseButton to="/about" variant="link" color="secondary" is-narrow>About</BaseButton>
+        <BaseButton to="/about" variant="solid" size="sm" color="secondary">About</BaseButton>
+        <BaseButton to="/about" variant="outline" size="sm" color="secondary">About</BaseButton>
+        <BaseButton to="/about" variant="text" size="sm" color="secondary" is-narrow>About</BaseButton>
+        <BaseButton to="/about" variant="link" size="sm" color="secondary">About</BaseButton>
       </div>
     </div>
   </main>
@@ -54,9 +57,30 @@ useHead({
     'welcome welcome welcome welcome'
     'sidebar text text text';
 
+  .profile-image {
+    grid-column: 1 / span 2;
+    grid-row: 1 / span 2;
+    z-index: 1;
+
+    :deep(img) {
+      margin-left: var(--sizing-xxxl);
+      border-radius: 100px 2px;
+      mix-blend-mode: multiply;
+      opacity: 0.25;
+    }
+  }
   .home-hello {
     grid-area: welcome;
     text-transform: uppercase;
+    color: var(--color-primary);
+    z-index: 2;
+    align-self: self-end;
+  }
+
+  .home-greeting {
+    grid-column: 2 / span 3;
+    grid-row: 2;
+    z-index: 2;
   }
 
   .content-area {
