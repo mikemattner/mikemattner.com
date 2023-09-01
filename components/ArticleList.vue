7b1d93eadb9ca3b37090e7d453a8edd6637ebebe
mikemattner <info@mikemattner.com>
2023-09-01 17:21:53 -0400

    Moving article list and items to their own components.

---
+++ components/ArticleList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="article-list">
+    <li v-for="post in posts" :key="post.title">
+      <ArticleListItem :post="post" />
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  posts: { type: Array as PropType<Post[]>, required: true },
+});
+</script>
+
+<style lang="scss" scoped>
+.article-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  li {
+    border-top: 1px solid var(--border-color);
+    border-bottom: 1px solid var(--border-color);
+  }
+}
+</style>
