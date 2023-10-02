41e110b77624f34ff1dfd560604c595c79031121
mikemattner <info@mikemattner.com>
2023-10-01 20:15:17 -0400

    Updating header nav, fixing a typo.

--- components/BaseNavigation.vue
+++ components/BaseNavigation.vue
@@ -3,7 +3,9 @@
     <div class="base-navigation__container">
       <NuxtLink to="/" class="brand-link"><BaseLogo show-label /></NuxtLink>
 
-      <nav class="base-navigation__controls">
+      <BaseNavigationButton :nav-open="navOpen" @toggle="toggleNav" />
+
+      <nav class="base-navigation__controls" :class="{ active: navOpen }">
         <ul class="navigation-list">
           <li class="navigation-list__item" v-for="item in navigationList" :key="item.title">
             <NuxtLink
@@ -47,13 +49,18 @@ const navigationList: NavigationList[] = [
     url: '/about',
   },
 ];
+
+const navOpen = ref<boolean>(false);
+
+const toggleNav = () => {
+  navOpen.value = !navOpen.value;
+};
 </script>
 
 <style lang="scss" scoped>
 .base-navigation {
   padding: 1.5rem 2rem;
   border-bottom: 1px solid var(--color-light-border-t);
-  mix-blend-mode: difference;
 
   @media (min-width: 716px) {
     grid-area: navigation;
@@ -62,11 +69,13 @@ const navigationList: NavigationList[] = [
     z-index: 1000;
     padding: var(--sizing-md);
     border: none;
+    mix-blend-mode: difference;
   }
 
   @media (max-width: 715px) {
     padding: 1rem;
     flex-direction: column;
+    background: var(--background-border-color);
   }
 
   &__container {
@@ -76,16 +85,18 @@ const navigationList: NavigationList[] = [
     margin-inline: auto;
     gap: var(--sizing-xl);
 
-    @media (min-width: 651px) {
+    @media (min-width: 716px) {
       justify-content: space-between;
       align-items: center;
       position: sticky;
       top: 0;
     }
 
-    @media (max-width: 650px) {
-      flex-direction: column;
-      justify-content: center;
+    @media (max-width: 715px) {
+      justify-content: space-between;
+      background: var(--background-border-color);
+      // flex-direction: column;
+      // justify-content: center;
     }
 
     @media (min-width: 716px) {
@@ -104,30 +115,7 @@ const navigationList: NavigationList[] = [
     line-height: 1;
     display: flex;
     align-items: center;
-  }
-
-  &__controls {
-    // display: grid;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    gap: var(--sizing-xxl);
-    grid-template-columns: 2fr auto;
-    flex-grow: 1;
-
-    @media (max-width: 650px) {
-      width: 100%;
-    }
-
-    // .theme-toggle {
-    //   align-self: self-end;
-    // }
-  }
-
-  &__sub-controls {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+    mix-blend-mode: difference;
   }
 
   .navigation-list {
@@ -139,10 +127,25 @@ const navigationList: NavigationList[] = [
     list-style: none;
     font-size: var(--size-step--1);
     font-family: var(--code-font-family);
+    mix-blend-mode: difference;
+
+    @media (max-width: 715px) {
+      flex-direction: column;
+      align-items: flex-end;
+      justify-content: flex-start;
+      font-size: var(--size-step-1);
+      gap: var(--sizing-sm);
+    }
 
     &__item {
       margin: 0;
       padding: 0;
+
+      @media (max-width: 715px) {
+        transform: translateY(-20px);
+        transition: var(--transition-cubic);
+        opacity: 0;
+      }
 
       &-link {
         display: flex;
@@ -164,6 +167,79 @@ const navigationList: NavigationList[] = [
   .router-link-active,
   .active-path {
     text-decoration-color: var(--color-light);
+  }
+
+  &__sub-controls {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    mix-blend-mode: difference;
+
+    @media (max-width: 715px) {
+      justify-content: flex-end;
+      transform: translateY(-20px);
+      transition: var(--transition-cubic);
+      opacity: 0;
+    }
+  }
+
+  &__controls {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: var(--sizing-xxl);
+    grid-template-columns: 2fr auto;
+    flex-grow: 1;
+
+    @media (max-width: 715px) {
+      z-index: 999;
+      position: fixed;
+      top: 0;
+      padding: 4.5rem 1.25rem 2.5rem;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      width: 100%;
+      background-color: var(--background-color-t);
+      backdrop-filter: blur(10px);
+      flex-direction: column;
+      align-items: flex-end;
+      justify-content: flex-start;
+      transition: all 0.125s 0.5s ease-in-out, clip-path 0.75s;
+      transform: translateY(-50vh);
+      clip-path: circle(25% at 150% -50%);
+      opacity: 0;
+      visibility: hidden;
+
+      &.active {
+        transform: translateY(0);
+        opacity: 1;
+        visibility: visible;
+        transition: var(--transition), clip-path 0.75s;
+        clip-path: circle(100%);
+
+        .navigation-list__item {
+          opacity: 1;
+          transform: translateY(0);
+
+          &:nth-child(1) {
+            transition: all 0.25s 0.375s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+          }
+          &:nth-child(2) {
+            transition: all 0.25s 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+          }
+          &:nth-child(3) {
+            transition: all 0.25s 0.625s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+          }
+        }
+
+        .base-navigation__sub-controls {
+          opacity: 1;
+          transform: translateY(0);
+          transition: all 0.25s 0.75s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+        }
+      }
+    }
   }
 }
 </style>
