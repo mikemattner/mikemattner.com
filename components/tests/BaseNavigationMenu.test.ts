0b7044c4c8bd2423fe50ed957ea56f6f0123af45
Mike Mattner <mikemattner@gmail.com>
2024-11-27 20:47:44 -0500

    Adding additional tests.

---
+++ components/tests/BaseNavigationMenu.test.ts
@@ -0,0 +1,50 @@
+import { mountSuspended } from '@nuxt/test-utils/runtime';
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import BaseNavigationMenu from '@/components/BaseNavigationMenu.vue';
+import { useNavigationState } from '@/composables/useNavigationState.client';
+import { navigationList } from '@/data/navigationList';
+import { nextTick } from 'vue';
+
+vi.mock('@/composables/useNavigationState.client', () => ({
+  useNavigationState: vi.fn(),
+}));
+
+describe('BaseNavigationMenu.vue', () => {
+  let navOpen;
+  let toggleNav: Function;
+
+  beforeEach(() => {
+    navOpen = ref(false);
+    toggleNav = vi.fn();
+    useNavigationState.mockReturnValue({ navOpen, toggleNav });
+  });
+
+  it('renders correctly', async () => {
+    const wrapper = await mountSuspended(BaseNavigationMenu);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders navigation list items', async () => {
+    const wrapper = await mountSuspended(BaseNavigationMenu);
+    const items = wrapper.findAll('.navigation-list__item');
+    expect(items.length).toBe(navigationList.length);
+  });
+
+  // TODO: figure out how to properly trigger these click events
+
+  it.skip('calls toggle function when a navigation item is clicked', async () => {
+    const wrapper = await mountSuspended(BaseNavigationMenu);
+    const firstItem = wrapper.find('.navigation-list__item div');
+    await firstItem.trigger('click');
+    await nextTick();
+    expect(toggleNav).toHaveBeenCalled();
+  });
+
+  it.skip('changes navOpen state when toggle function is called', async () => {
+    const wrapper = await mountSuspended(BaseNavigationMenu);
+    const button = wrapper.findComponent({ name: 'BaseNavigationButton' });
+    await button.trigger('click');
+    await nextTick();
+    expect(toggleNav).toHaveBeenCalled();
+  });
+});
