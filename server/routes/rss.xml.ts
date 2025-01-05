7eb86f493de209304d790474e04b07583427528c
Mike Mattner <mikemattner@gmail.com>
2025-01-05 16:51:11 -0500

    Update some rss items.

--- server/routes/rss.xml.ts
+++ server/routes/rss.xml.ts
@@ -1,5 +1,6 @@
 import { serverQueryContent } from '#content/server';
 import RSS from 'rss';
+import { toHtml } from 'hast-util-to-html';
 
 export default defineEventHandler(async (event) => {
   const feed = new RSS({
@@ -9,9 +10,31 @@ export default defineEventHandler(async (event) => {
   });
 
   const docs = await serverQueryContent(event).sort({ date: -1 }).where({ _partial: false }).find();
-  // const blogPosts = docs.filter((doc) => doc?._path?.includes('/blog'));
+  const recursivelyPatchChildren = (node: any) => {
+    if (node.type === 'text') {
+      return node;
+    } else if (node.tag === 'code' && node.props.language) {
+      node.props.lang = node.props.language;
+      node.children = [{ type: 'text', value: node.props.code }];
+      delete node.props.language;
+      delete node.props.code;
+    }
+    node.tagName = node.tag;
+    node.properties = node.props;
+    node.children = node.children.map(recursivelyPatchChildren);
+    return node;
+  };
 
   for (const doc of docs) {
+    // TODO: This is a hack to get the content to render correctly. Update to eventually use this.
+    let content;
+    if (doc.body) {
+      doc.body.children = doc.body.children.map(recursivelyPatchChildren);
+      content = toHtml(doc.body as any);
+    } else {
+      content = doc.description;
+    }
+
     feed.item({
       title: doc.title ?? '-',
       url: `https://mikemattner.com${doc._path}`,
